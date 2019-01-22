15363b4ec36274dda8f9402c2e8b5f76b3814f8d
Bryan Terce <bryan.terce@gmail.com>
2019-01-22 12:20:07 -0800

    Give server time to shutdown, paranoid

--- src/entry/shutdown.js
+++ src/entry/shutdown.js
@@ -2,6 +2,7 @@ import AWS from "aws-sdk"
 
 import RconClient from "modern-rcon"
 import pinger from "minecraft-pinger"
+import delay from "delay"
 
 const ec2 = new AWS.EC2({
   apiVersion: "2016-11-15"
@@ -14,12 +15,19 @@ export default async () => {
     const rcon = new RconClient(process.env.SERVER_HOST, Number(process.env.RCON_PORT), process.env.RCON_PASSWORD)
 
     await rcon.connect()
-    const response = await rcon.send("stop")
-    console.log(response)
+    await sendCommand(rcon, "save-all")
+    await sendCommand(rcon, "stop")
     await rcon.disconnect()
+
+    await delay(5000)
 
     await ec2.stopInstances({
       InstanceIds: [process.env.INSTANCE_ID]
     }).promise()
   }
+}
+
+async function sendCommand(rcon, command) {
+  const response = await rcon.send(command)
+  console.log(response)
 }
